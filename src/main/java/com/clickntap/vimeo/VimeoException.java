7d57974e5743b16f4123385732ca6d27ae9e2dfe
Jamie Townsend <jamie_townsend@hotmail.com>
2021-07-30 17:02:01 +0200

    #3 Prepare first release with new GAV coordinates

--- src/main/java/com/clickntap/vimeo/VimeoException.java
+++ src/main/java/com/clickntap/vimeo/VimeoException.java
@@ -2,6 +2,11 @@ package com.clickntap.vimeo;
 
 public class VimeoException extends Exception {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
     public VimeoException(String message) {
         super(message);
     }
